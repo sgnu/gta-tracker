206b93be37d243bbfe4f4a51521e1df68219b2bf
sgnu <sam.nguyen.1223@gmail.com>
2020-05-20 14:56:34 -0400

    Change vehicle rendering to using array

--- src/Components/VehicleCargoTracker.js
+++ src/Components/VehicleCargoTracker.js
@@ -24,19 +24,15 @@ class VehicleCargoTracker extends React.Component {
   }
 
   render() {
+    const standardVehicles = standardRangeCars.map((vehicle) => {
+      return <Vehicle key = { vehicle } vehicleName = { vehicle } />
+    });
+
     return (
       <div className="VehicleCargoTracker">
         <h1>Standard Range</h1>
-        <Vehicle vehicleName={standardRangeCars[0]} enabled={ this.state.standardRange[0] } id={0} />
-        <Vehicle vehicleName={standardRangeCars[1]} enabled={ this.state.standardRange[1] } id={1} />
-        <Vehicle vehicleName={standardRangeCars[2]} enabled={ this.state.standardRange[2] } id={2} />
-        <Vehicle vehicleName={standardRangeCars[3]} enabled={ this.state.standardRange[3] } id={3} />
-        <Vehicle vehicleName={standardRangeCars[4]} enabled={ this.state.standardRange[4] } id={4} />
-        <Vehicle vehicleName={standardRangeCars[5]} enabled={ this.state.standardRange[5] } id={5} />
-        <Vehicle vehicleName={standardRangeCars[6]} enabled={ this.state.standardRange[6] } id={6} />
-        <Vehicle vehicleName={standardRangeCars[7]} enabled={ this.state.standardRange[7] } id={7} />
-        <Vehicle vehicleName={standardRangeCars[8]} enabled={ this.state.standardRange[8] } id={8} />
-        <Vehicle vehicleName={standardRangeCars[9]} enabled={ this.state.standardRange[9] } id={9} />
+        { standardVehicles }
+
       </div>
     );
   }
