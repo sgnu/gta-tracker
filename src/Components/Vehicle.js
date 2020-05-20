b6db667b4e371ebcf441816c04a46cd3a5f7976d
sgnu <sam.nguyen.1223@gmail.com>
2020-05-20 14:51:17 -0400

    Add standard range vehicles

--- src/Components/Vehicle.js
+++ src/Components/Vehicle.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class Vehicle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enabled: this.props.enabled
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState(state => ({ enabled: !this.state.enabled }));
+    this.setState(state => ({ color: this.state.enabled ? '#000000' : '#00000080' }));
+  }
 
   render() {
     return (
-      <div className="Vehicle">
-        <h2>{ this.props.vehicleName }</h2>
+      <div className="Vehicle" onClick={this.handleClick}>
+        <h2 style={{ color: this.state.color }}>{ this.props.vehicleName }</h2>
       </div>
     )
   }
--- src/Components/VehicleCargoTracker.js
+++ src/Components/VehicleCargoTracker.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './VehicleCargoTracker.css'
+import Vehicle from './Vehicle'
 
 const standardRangeCars = [
   "Alpha",
-  "Banshee 900R"
+  "Banshee 900R",
+  "Bestia GTS",
+  "Feltzer",
+  "Jester",
+  "Massacro",
+  "Nightshade",
+  "Sabre Turbo Custom",
+  "Tampa",
+  "Turismo R"
 ]
 
 class VehicleCargoTracker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      standardRange: '00'
+      standardRange: [false, false, false, false, false, false, false, false, false, false]
     };
   }
 
@@ -18,7 +27,16 @@ class VehicleCargoTracker extends React.Component {
     return (
       <div className="VehicleCargoTracker">
         <h1>Standard Range</h1>
-        <h2>{ standardRangeCars[0] }</h2>
+        <Vehicle vehicleName={standardRangeCars[0]} enabled={ this.state.standardRange[0] } id={0} />
+        <Vehicle vehicleName={standardRangeCars[1]} enabled={ this.state.standardRange[1] } id={1} />
+        <Vehicle vehicleName={standardRangeCars[2]} enabled={ this.state.standardRange[2] } id={2} />
+        <Vehicle vehicleName={standardRangeCars[3]} enabled={ this.state.standardRange[3] } id={3} />
+        <Vehicle vehicleName={standardRangeCars[4]} enabled={ this.state.standardRange[4] } id={4} />
+        <Vehicle vehicleName={standardRangeCars[5]} enabled={ this.state.standardRange[5] } id={5} />
+        <Vehicle vehicleName={standardRangeCars[6]} enabled={ this.state.standardRange[6] } id={6} />
+        <Vehicle vehicleName={standardRangeCars[7]} enabled={ this.state.standardRange[7] } id={7} />
+        <Vehicle vehicleName={standardRangeCars[8]} enabled={ this.state.standardRange[8] } id={8} />
+        <Vehicle vehicleName={standardRangeCars[9]} enabled={ this.state.standardRange[9] } id={9} />
       </div>
     );
   }
